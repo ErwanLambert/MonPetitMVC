34683f5506fa52a7338750b38a356e8d8dd4b4fd
ErwanLambert <erwanlambert@hotmail.fr>
2021-10-20 10:26:28 +0200

    cherche unClient

--- parametres.php
+++ parametres.php
@@ -1,6 +1,6 @@
 <?php
 define('DATABASE_URL', "localhost" );
-define('DATABASE_USER', "lambert/lambert");
-define('DATABASE_PWD', "lambert");
-define('DATABASE_NAME', "clicom");
+define('DATABASE_USER', "root");
+define('DATABASE_PWD', "");
+define('DATABASE_NAME', "clicommvc");
 define('CNSTRING', "mysql:host=" . DATABASE_URL . ";dbname=" . DATABASE_NAME .";charset=UTF8" );
